c406aafd41c01b8fc398354b11c4026e8fa9c91f
ak1lucky <senya.gioco@mail.ru>
2023-09-28 17:16:25 +0300

    Elevators moovement + reset buttons

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -9,7 +9,7 @@
               <div class="highlighter" :style="`top: ${shaft.top}px; height: ${settings.floorBox}px; width: ${settings.floorBox}px`">
                   {{ Math.round((settings.floors * settings.floorBox  - shaft.top) / settings.floorBox) }}
               </div>
-              <button class="reset" :style="`width: ${settings.floorBox}px`">
+              <button class="reset" @click="reset(index1)" :style="`width: ${settings.floorBox}px`">
                   Reset
               </button>
           </div>
@@ -33,19 +33,41 @@ export default{
         floors: 15,
         floorBox: 20,
       },
-      shafts:[]
+      shafts:[],
+    };
+  },
+  mounted() {
+    for (let i = 0; i < this.settings.shafts; i++) {
+      const newFloor =
+        Math.round(Math.random() * (this.settings.floors - 1)) + 1;
+      this.shafts.push({
+        floor: newFloor,
+        top: this.settings.floorBox * (this.settings.floors - newFloor),
+        loader: false,
+      });
     }
   },
-  mounted(){
-    for (let i = 0; i < this.settings.shafts; i++) {
-        const newFloor = Math.round(Math.random()* (this.settings.floors - 1)) + 1
-        this.shafts.push({
-            floor: newFloor,
-            top: this.settings.floorBox * (this.settings.floors - newFloor),
-            loader: false
-        })
-    }
-  }  
+  methods:{
+    async shiftFloor(index, to, amount){
+      let step = (this.shafts[index].top - to) / (20 * amount)
+      if (step === 0)  {
+          this.shafts[index].loader = false
+          return
+      }
+      for (let i = 0; i < 20 * amount; i++) {
+          await new Promise(resolve => setTimeout(resolve, 10));
+          this.shafts[index].top -= step
+      }
+      this.shafts[index].loader = false
+    },
+    async reset(index){
+      if (this.shafts[index].loader) return;
+      this.shafts[index].loader = true
+      await this.shiftFloor(index, (this.settings.floors - 1) * this.settings.floorBox, (this.shafts[index].floor - 1)); 
+      this.shafts[index].floor = 1
+      this.shafts[index].loader = false
+    },
+  }
 }
 </script>
 
